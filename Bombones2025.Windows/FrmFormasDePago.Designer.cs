584d5512ba400cff46ce2cccd5fb181be46e6a79
BiancaLopresto <bianlopre19@gmail.com>
2025-05-27 20:04:51 -0300

    Creacion del formulario secundario

---
+++ Bombones2025.Windows/FrmFormasDePago.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Bombones2025.Windows
+{
+    partial class FrmFormasDePago
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            txtFormaDePago = new TextBox();
+            BtnCancelar = new Button();
+            BtnOK = new Button();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(32, 40);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Forma De Pago:";
+            // 
+            // txtFormaDePago
+            // 
+            txtFormaDePago.Location = new Point(129, 37);
+            txtFormaDePago.Name = "txtFormaDePago";
+            txtFormaDePago.Size = new Size(340, 23);
+            txtFormaDePago.TabIndex = 1;
+            // 
+            // BtnCancelar
+            // 
+            BtnCancelar.Image = Properties.Resources.Cancelar;
+            BtnCancelar.Location = new Point(394, 128);
+            BtnCancelar.Name = "BtnCancelar";
+            BtnCancelar.Size = new Size(75, 52);
+            BtnCancelar.TabIndex = 9;
+            BtnCancelar.Text = "Cancelar";
+            BtnCancelar.TextImageRelation = TextImageRelation.ImageAboveText;
+            BtnCancelar.UseVisualStyleBackColor = true;
+            BtnCancelar.Click += BtnCancelar_Click;
+            // 
+            // BtnOK
+            // 
+            BtnOK.Image = Properties.Resources.Aceptar1;
+            BtnOK.Location = new Point(55, 128);
+            BtnOK.Name = "BtnOK";
+            BtnOK.Size = new Size(75, 52);
+            BtnOK.TabIndex = 10;
+            BtnOK.Text = "OK";
+            BtnOK.TextImageRelation = TextImageRelation.ImageAboveText;
+            BtnOK.UseVisualStyleBackColor = true;
+            BtnOK.Click += BtnOK_Click;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // FrmFormasDePago
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(592, 232);
+            Controls.Add(BtnCancelar);
+            Controls.Add(BtnOK);
+            Controls.Add(txtFormaDePago);
+            Controls.Add(label1);
+            MaximumSize = new Size(608, 271);
+            MinimumSize = new Size(608, 271);
+            Name = "FrmFormasDePago";
+            Text = "FrmFormasDePago";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtFormaDePago;
+        private Button BtnCancelar;
+        private Button BtnOK;
+        private ErrorProvider errorProvider1;
+    }
+}
